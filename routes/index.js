8ec29b9ab48cf0858f22f456515983bff89fb34e
Your Name <pedrohenrique2604@gmail.com>
2018-06-01 20:29:06 -0300

    Criar substituições nos templates. Só falta highlights.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,44 @@
 var express = require('express');
 var router = express.Router();
 
+// stores Helicopterson's info
+const helicoptersonInfo = {
+	name: 'Helicopterson dos Santos',
+	role: 'Escritor, poeta, filantropo',
+  	birth: '1870',
+  	death: '1950',
+  	background: 'Helicopterson sempre foi um jovem diferenciado, no mundo da lua, com aspirações muito além de sua geração. Com todos os seus sonhos e com muito esforço, um dia aprendeu a voar. Isso, ele aprendeu a voar, fazendo justiça à seu nome tão único. Ele continuou se esforçando, aprendendo muito e dominando a arte do voô, sempre chegando a alturas cada vez maiores. Até que um dia chegou na Lua, e essa na verdade é a última notícia que se tem dele. Depois disso, ele continuou voando cada vez mais alto, e ninguém mais o viu.' ,
+  	photo: '/images/helicopterson.png',
+  	quote: 'Se não for pra voar, eu nem desço pro Play',
+  	highlights: ['Nasceu', 'Voou', 'Voou mais alto', 'Primeiro homem a pisar na lua', 'Sumiu'],
+};
+
+// stores Lincoln's info
+const lincolnInfo = {
+    name: 'Abraham Lincoln',
+    role: 'Ex-presidente, advogado',
+    birth: '1809',
+    death: '1865',
+    background: 'Abraham Lincoln foi um político norte-americano que serviu como o 16° presidente dos Estados Unidos, posto que ocupou de 4 de março de 1861 até seu assassinato em 15 de abril de 1865. Lincoln liderou o país de forma bem-sucedida durante sua maior crise interna, a Guerra Civil Americana, preservando a União e abolindo a escravidão, fortalecendo o governo nacional. Lincoln tem sido consistentemente considerado por estudiosos e pelo povo como um dos três maiores presidentes dos Estados Unidos.',
+    photo: '/images/lincoln.png',
+    quote: 'Governo das pessoas, pelas pessoas, para as pessoas, nunca deixará de existir na Terra.',
+    highlights: ['Aprovou a Lei da Propriedade Rural', 'Emitiu a Proclamação de Emancipação, que levou à abolição da escravatura nos EUA', 'Liderou a União à vitória da Guerra Civil'],
+};
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Meu primeiro WebApp Incrível' });
 });
 
-/* GET test page. */
+/* GET Helicopterson's page. */
 router.get('/helicopterson', function(req, res, next) {
-  res.render('bio');
+  res.render('bio', helicoptersonInfo);
+});
+
+/* GET Licoln's page. */
+router.get('/lincoln', function(req, res, next) {
+  res.render('bio', lincolnInfo);
 });
 
 module.exports = router;
