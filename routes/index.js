f14a5bb1587d13405d303ef074ff4bcfb9e66162
Your Name <pedrohenrique2604@gmail.com>
2018-06-01 19:11:52 -0300

    Construir HTML e CSS.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Meu primeiro WebApp Incrível' });
+});
+
+/* GET test page. */
+router.get('/helicopterson', function(req, res, next) {
+  res.render('bio');
 });
 
 module.exports = router;
